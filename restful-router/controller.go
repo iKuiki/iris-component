62365c3812c8181fe663626cf0d135335b392ad5
kuiki <911yinhui911@163.com>
2017-04-27 00:06:28 +0800

    add RESTful router Register for iris.v6

---
+++ restful-router/controller.go
@@ -0,0 +1,23 @@
+package restful
+
+import (
+	"gopkg.in/kataras/iris.v6"
+)
+
+type Controller struct {
+}
+
+func (this *Controller) Index(ctx *iris.Context) {
+}
+
+func (this *Controller) Store(ctx *iris.Context) {
+}
+
+func (this *Controller) Show(ctx *iris.Context, id string) {
+}
+
+func (this *Controller) Update(ctx *iris.Context, id string) {
+}
+
+func (this *Controller) Destroy(ctx *iris.Context, id string) {
+}
