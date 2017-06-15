4e057a11777161b1694e9fba8ef7445dcc8a1ab4
kuiki <911yinhui911@163.com>
2017-06-15 16:33:43 +0800

    if function not imply, return 404 not found at restful controller

--- restful-router/controller.go
+++ restful-router/controller.go
@@ -8,16 +8,21 @@ type Controller struct {
 }
 
 func (this *Controller) Index(ctx *iris.Context) {
+	ctx.NotFound()
 }
 
 func (this *Controller) Store(ctx *iris.Context) {
+	ctx.NotFound()
 }
 
 func (this *Controller) Show(ctx *iris.Context, id string) {
+	ctx.NotFound()
 }
 
 func (this *Controller) Update(ctx *iris.Context, id string) {
+	ctx.NotFound()
 }
 
 func (this *Controller) Destroy(ctx *iris.Context, id string) {
+	ctx.NotFound()
 }
